741d431ea32edb196dffe09da408dc25495cbe59
AlexisTech41 <solucionestechmx@gmail.com>
2023-12-14 12:23:08 -0600

    Actualizacion Herencia y polimorfismo, se agrega clase base vehiculo

--- procesar.php
+++ procesar.php
@@ -1,30 +1,51 @@
 <?php
 
-// Definición de la clase Moto
-class Moto {
-    // Propiedades de la clase
+// Definición de la clase Vehiculo (Clase Base)
+class Vehiculo {
+    // Propiedades de la clase base
     public $cc;
     public $color;
     public $potencia;
-    public $marca;
-    public $modelo;
 
-    // Constructor de la clase
-    public function __construct($cc, $color, $potencia, $marca, $modelo) {
+    // Constructor de la clase base
+    public function __construct($cc, $color, $potencia) {
         // Inicialización de las propiedades
         $this->cc = $cc;
         $this->color = $color;
         $this->potencia = $potencia;
+    }
+
+    // Método para mostrar la configuración del vehículo
+    public function mostrarConfiguracion() {
+        echo "<h2>Configuración del Vehículo</h2>";
+        echo "<p>Cilindrada (CC): {$this->cc}</p>";
+        echo "<p>Color: {$this->color}</p>";
+        echo "<p>Potencia: {$this->potencia}</p>";
+    }
+}
+
+// Definición de la clase Moto que hereda de Vehiculo
+class Moto extends Vehiculo {
+    // Propiedades específicas de la clase Moto
+    public $marca;
+    public $modelo;
+
+    // Constructor de la clase Moto
+    public function __construct($cc, $color, $potencia, $marca, $modelo) {
+        // Llama al constructor de la clase base
+        parent::__construct($cc, $color, $potencia);
+
+        // Inicialización de las propiedades específicas de Moto
         $this->marca = $marca;
         $this->modelo = $modelo;
     }
 
     // Método para mostrar la configuración de la moto
     public function mostrarConfiguracion() {
-        echo "<h2>Configuración de la Moto</h2>";
-        echo "<p>Cilindrada (CC): {$this->cc}</p>";
-        echo "<p>Color: {$this->color}</p>";
-        echo "<p>Potencia: {$this->potencia}</p>";
+        // Llama al método de la clase base
+        parent::mostrarConfiguracion();
+
+        // Muestra las propiedades específicas de Moto
         echo "<p>Marca: {$this->marca}</p>";
         echo "<p>Modelo: {$this->modelo}</p>";
     }
